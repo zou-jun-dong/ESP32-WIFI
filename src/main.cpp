014b603c63f7d795c835f9993bdf1b1628180eb6
zou-jun-dong <20250070118@stu.fosu.edu.cn>
2026-04-30 18:53:54 +0800

    feat(network): implement Access Point (AP) mode - Configured ESP32 to operate in AP mode, broadcasting its own SSID. - Set up network credentials (SSID and password) for the soft AP. - Implemented serial output to display the AP's default IP address (usually 192.168.4.1).

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,39 @@
 #include <WiFi.h>  //include the WI-FI library for ESP32
-#include "secrets.h"  //Include the header file with Wi-Fi credentials
 #include <Arduino.h>
-#include <WebServer.h> //Include the Webserver library
-//Create a WebServer instance on port 80
-WebServer server(80);
-//Function to handle requests to thr root path
-void handleRoot(){
-  //Define the HTML content to be displayed
-  String html="<html><head><metacharset='UTF-8'></head><body>";
-  html+="<h1>Hello AIoT!</h1>";
-  html+="<p>Current Light Status: <b>ON</b></p>";
-  html+="</body></html>";
 
-  //Send HTTP 200 response with the HTML content
-  server.send(200,"text/html",html);
-}
+//Set the SSID and password for the ESP32 Access Point
+const char* AP_SSID="ESP32_AIoT_AP";
+const char* AP_PASS="12345678";
+
+
 void setup(){
   //Initialize serial communication at 115200 baud rate
   Serial.begin(115200);
   //Short delay to ensure the serial monitor is ready
   delay(1000);
   Serial.println("\n----------------------------------------");
-  Serial.print("Connecting to ");
-  Serial.println(WIFI_SSID);
-  //Set Wi-Fi mode to Station (STA),acting as a client to connect to the router
-  WiFi.mode(WIFI_STA);
-  //Start connecting to Wi-Fi with credentials
-  WiFi.begin(WIFI_SSID,WIFI_PASSWORD);
-  //Cpntinuously check Wi-Fi connection status yntil successfully connnected
-  //WiFi.status() returns the current connection status
-  while (WiFi.status()!=WL_CONNECTED)
+  Serial.println("Congiguring Access Point...");
+  //Set Wi-Fi mode to Access Point (AP),turningt the device into a hotpot
+  WiFi.mode(WIFI_AP);
+  //Start the Access Point
+  bool result=WiFi.softAP(AP_SSID,AP_PASS);
+  if (result)
   {
     /* code */
-    delay(500);  //Wait 500 milliseconds between checks
-    Serial.print("."); //Print a dot to indicate it is tring to connect
-
+    Serial.println("AP Mode started successfully!");
+      //Get the IP adress of the ESP32 AP (default is usually 192.168.4.1)
+  IPAddress IP=WiFi.softAPIP();
+  Serial.print("AP Ip Address");
+  Serial.print(IP);
   }
-  //Exiting the loop means the connection was successful
-  Serial.println("");
-  Serial.println("Wi-Fi connectedd successfully!");
-  //Get and print the local IP address assigned bt the router
-  Serial.print("IP Adderess ");
-  Serial.println(WiFi.localIP());
-  Serial.println("----------------------------------------");
-  //Web Server Configuration
-  
-  //When a user accesses the root path,execute the handleRoot function
-  server.on("/",handleRoot);
+   else
+   {
+    Serial.println("Failled to start AP Mode");
 
-  //Start the server
-  server.begin();
-  Serial.println("HTTP Server started.");
+   }
+   Serial.println("-------------------------------------------");
 }
 
 void loop(){
-  //Continuously handle incoming requests from clients(btowsers)
-  server.handleClient();
+   //Your ESP32 is now continuously broadcasting its Wi-Fi signal
 }
